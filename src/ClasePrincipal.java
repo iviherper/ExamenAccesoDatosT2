28bd647808e0c2529ab316d3c968445d2c3d6dd1
Ivan <iherper97@hotmail.com>
2021-03-09 15:58:23 +0100

    init

--- src/ClasePrincipal.java
+++ src/ClasePrincipal.java
@@ -2,7 +2,25 @@
 public class ClasePrincipal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		boolean continu = true;
+		while (continu) {
+			int opcion = EntradaSalida.pedirOpcion();
+			switch (opcion) {
+			case EntradaSalida.BUSCARPALABRA:
+				
+				break;
+			case EntradaSalida.LISTARPALABRAS:
+				break;
+			case EntradaSalida.VOLCARDATOS:
+				break;
+			case EntradaSalida.SALIR:
+				continu=false;
+				break;
+			default:
+				System.out.println("Introduce una opcion valida");
+				break;
+			}
+		}
 
 	}
 
--- src/EntradaSalida.java
+++ src/EntradaSalida.java
@@ -1,4 +1,21 @@
+import java.util.Scanner;
 
 public class EntradaSalida {
+
+	public static final int SALIR = 4;
+	public static final int VOLCARDATOS = 3;
+	public static final int BUSCARPALABRA = 1;
+	public static final int LISTARPALABRAS = 2;
+	private static Scanner sc = new Scanner(System.in);
+	
+	public static int pedirOpcion() {
+		System.out.println("Introduce una opción:");
+		System.out.println("1-Buscar palabra");
+		System.out.println("2-Listar palabras");
+		System.out.println("3-Volcar datos de MongoDB en MySQL");
+		System.out.println("4-Salir");
+		int opcion = Integer.parseInt(sc.nextLine());
+		return opcion;
+	}
 
 }
